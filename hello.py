5fb98a93ebeb70b8ac5e48aea1351da6cab40cac
sbhatia9397 <58414720+sbhatia9397@users.noreply.github.com>
2019-12-26 10:42:23 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,4 @@
-import sys
+def test():
+    return("hello world")
 
-def Factorial(n): # return factorial
-    result = 1
-    for i in range (1,n):
-        result = result * i
-    print "factorial is ",result
-    return result
-
-print Factorial(10)
+test()
